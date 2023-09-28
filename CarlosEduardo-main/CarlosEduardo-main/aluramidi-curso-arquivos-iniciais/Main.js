9535fa46d55e2c2b08ba38ba919ec8c0d5a71268
CaRlOSeDuArDocontatato <113601016+CaRlOSeDuArDocontatato@users.noreply.github.com>
2023-09-28 08:55:23 -0300

    Add files via upload

---
+++ CarlosEduardo-main/CarlosEduardo-main/aluramidi-curso-arquivos-iniciais/Main.js
@@ -0,0 +1,20 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
